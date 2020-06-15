25fcf629fedb5f79330f9b34d6ec949ac7f38b28
chendante <danteliuzichen@163.com>
2020-06-16 04:33:21 +0800

    test proc

--- hello/hello.c
+++ hello/hello.c
@@ -9,38 +9,39 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 int hello_count = 0;
 
-int	hello_proc_read (struct seq_file *s, void *v)
+int hello_proc_read(struct seq_file *s, void *v)
 {
     seq_printf(s, "hello_count:%d\n", hello_count);
-    printk(KERN_ALERT "Hello Count PRINT! BY ZICHEN LIU");
     return 0;
 }
 
-ssize_t hello_proc_write (struct file *filp, const char __user *buf, size_t size, loff_t *ppos)
+ssize_t hello_proc_write(struct file *filp, const char __user *buf, size_t size, loff_t *ppos)
 {
-    if(__get_user(hello_count, (int __user *)buf))
-        return -EFAULT;
-    hello_count = hello_count - '0';
-    printk(KERN_ALERT "Hello Count changed! BY ZICHEN LIU");
+    char tmpbuf[64];
+
+    if (buff && !copy_from_user(tmpbuf, buf, size))
+    {
+        hello_count = tmpbuf[0] - '0';
+    }
+
     return size;
 }
 
 static int hello_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, hello_proc_read, NULL);
+    return single_open(file, hello_proc_read, NULL);
 }
 
 static struct proc_ops hello_proc_ops = {
-    .proc_open    = hello_open,
-	.proc_read    = seq_read,
-    .proc_write   = hello_proc_write
-};
+    .proc_open = hello_open,
+    .proc_read = seq_read,
+    .proc_write = hello_proc_write};
 
 static int hello_init(void)
 {
     // printk(KERN_ALERT "Hello, world\n");
     proc_create("hello", 0 /* default mode */,
-			NULL /* parent dir */, &hello_proc_ops);
+                NULL /* parent dir */, &hello_proc_ops);
     return 0;
 }
 
